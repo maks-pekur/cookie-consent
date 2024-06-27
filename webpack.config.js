b5b1cbedae7eff4381b3fe1659653e87faa98d61
maks_pekur <maxpekur88@gmail.com>
2024-06-27 16:29:46 +0200

    Update script and bump version to 1.0.5

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,27 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
-	entry: './src/cookie-consent.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'cookie-consent.min.js',
-		library: 'CookieConsent',
-		libraryTarget: 'umd',
-		globalObject: 'this',
-		umdNamedDefine: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-		],
-	},
-}
+  entry: './src/cookie-consent.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'cookie-consent.min.js',
+    library: 'CookieConsent',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
